113dafa99ad4d1993ece2840d241dcfbfdf03f43
ArtArcanjo <arthurarcanjo14@gmail.com>
2025-08-29 13:50:17 -0300

    0.0.1 update

--- integra-ifood/src/auth/auth.service.ts
+++ integra-ifood/src/auth/auth.service.ts
@@ -2,23 +2,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { UsersService } from '../Users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private users: UsersService, private jwt: JwtService) {}
+  constructor() {}
 
-  async validateUser(email: string, password: string) {
-    const user = await this.users.findByEmail(email);
-    if (!user) throw new UnauthorizedException('Credenciais inválidas');
-    const ok = await bcrypt.compare(password, user.passwordHash);
-    if (!ok) throw new UnauthorizedException('Credenciais inválidas');
-    return user;
-  }
 
-  async login(email: string, password: string) {
-    const user = await this.validateUser(email, password);
-    const payload = { sub: user.id, email: user.email };
-    return { access_token: await this.jwt.signAsync(payload) };
-  }
 }
