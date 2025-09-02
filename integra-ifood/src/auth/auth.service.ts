f2abfe4ae4d1a3200d0735911fb7f1ff90c62009
ArtArcanjo <arthurarcanjo14@gmail.com>
2025-09-02 14:26:07 -0300

    hotfix encoded uri updateloc

--- integra-ifood/src/auth/auth.service.ts
+++ integra-ifood/src/auth/auth.service.ts
@@ -2,7 +2,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { UsersService } from '../Users/users.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
