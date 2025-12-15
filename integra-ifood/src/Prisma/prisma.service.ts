589a8d66f8d3345d31e8fadd95bd1adbc99f26d6
ArtArcanjo <arthurarcanjo14@gmail.com>
2025-12-15 17:04:39 -0300

    inventory

--- integra-ifood/src/Prisma/prisma.service.ts
+++ integra-ifood/src/Prisma/prisma.service.ts
@@ -455,7 +455,12 @@ async logoutSession(userEmail : string){
   
 async alterarSenha(userEmail : string, senha : string){
   const novaSenha = await bcrypt.hash(senha, 12);
-  //return prisma.user.update({where : {userEmail}, data{senha : novaSenha}});
+  return prisma.user.update({
+        where: { userEmail },
+        data: {
+          senha: { set: novaSenha },
+        },
+    })
 }
 
 async getLogins(){
