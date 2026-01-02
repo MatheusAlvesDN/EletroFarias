8cfeb8251a4cb1068bb699ea7644f3ac7183be12
ArtArcanjo <arthurarcanjo14@gmail.com>
2026-01-02 11:23:18 -0300

    inventory

--- integra-front/next.config.ts
+++ integra-front/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-
-  // Se você usa next/image, isso evita erro no export estático
-  images: { unoptimized: true },
-
-  // Ajuda muito quando a app roda assets localmente (file://) via WebView
-  assetPrefix: "./",
+  /* config options here */
 };
 
 export default nextConfig;
