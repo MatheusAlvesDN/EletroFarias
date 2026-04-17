365d44035a4b938e75fc1e4fd96eb404ea7df700
MatheusAlvesDN <matheus.nascimento@ccc.ufcg.edu.br>
2026-04-17 15:43:45 -0300

    EletroClube

--- integra-front/src/middleware.ts
+++ integra-front/src/middleware.ts
@@ -48,6 +48,6 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)",
   ],
 };
